512d4828c247df3c98f4873c3d498d04896fb758
Maxence Vahedi <maxence.vahedi@gmail.com>
2024-01-31 22:07:54 +0100

    random drop

--- Apple.gd
+++ Apple.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 var picked = false
 var player = null
 var just_dropped = false
+var a = 1 	# way the vector goes, 1 or -1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,7 +17,11 @@ func _process(delta):
 	elif just_dropped:
 		$CollisionShape2D.disabled = false
 		$PickArea/CollisionShape2D.disabled = false
-		apply_central_impulse(Vector2(100,0))
+		if randi() % 2 == 0:
+			a = 1
+		else:
+			a = -1
+		apply_central_impulse(Vector2(100 * a,0))
 		just_dropped = false		
 		
 func _input(event):
